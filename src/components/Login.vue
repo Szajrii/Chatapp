bf66ebaca541f3d7ac4877d446f1198d47d28658
Szajrii <michal05v@gmail.com>
2020-02-16 11:32:33 +0100

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="login">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
+        </div>
+        <button type="submit" class="btn btn-primary btn-lg chat-userhandler-button">Login</button>
+    </form>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    @Component
+    export default class Login extends Vue {
+        private login: string = '';
+        private password: string = '';
+
+        private logIn() {
+            this.$store.commit('setUser', this.login);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Start.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="chat-userhandler">
+        <div class="chat-userhandler-content">
+            <p class="text-primary">Do not wait any longer join chat app community and talk with anybody around the world!</p>
+            <router-view />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Start extends Vue {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
