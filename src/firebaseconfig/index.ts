59ae55de901bb486ff778c551299b58eaad9b1fc
Szajrii <michal05v@gmail.com>
2020-03-06 15:28:06 +0100

    magic

---
+++ src/firebaseconfig/index.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBghvS0Ba_aDyhY2TL8YVCsPZ9z2S0Todg",
+    authDomain: "chatapp-d2744.firebaseapp.com",
+    databaseURL: "https://chatapp-d2744.firebaseio.com",
+    projectId: "chatapp-d2744",
+    storageBucket: "chatapp-d2744.appspot.com",
+    messagingSenderId: "604094987613",
+    appId: "1:604094987613:web:a4e3556d7ac23e9c40bcb4"
+}
